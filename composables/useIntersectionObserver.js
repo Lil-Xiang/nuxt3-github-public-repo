80d478d2f96cabf0ca14eefcae32c253a22b472a
Xiang <joey861001@gmail.com>
2023-09-07 23:02:32 +0800

    0907

---
+++ composables/useIntersectionObserver.js
@@ -0,0 +1,32 @@
+export function useIntersectionObserver() {
+  const observer = ref(null);
+  const isIntersection = ref(false);
+  const elRef = ref(null);
+
+  const intersectionObserver = (
+    el,
+    options = {
+      root: null,
+      threshold: 0,
+    }
+  ) => {
+    elRef.value = el;
+    observer.value = new IntersectionObserver((entries) => {
+      entries.forEach((item) => {
+        isIntersection.value = item.isIntersecting;
+      });
+    }, options);
+
+    observer.value.observe(el);
+  };
+
+  const unobserver = () => {
+    if(observer.value) observer.value.unobserve(elRef.value);
+  }
+
+  onUnmounted(() => {
+    unobserver();
+  })
+
+  return { intersectionObserver, isIntersection, elRef };
+}
